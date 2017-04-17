8c50c44af360422af95547b76423afb43956ab8c
robinator8 <robertcraig38@gmail.com>
2017-04-17 16:07:54 -0400

    test

--- application.py
+++ application.py
@@ -29,7 +29,7 @@ db = SQL("sqlite:///finance.db")
 
 @app.route("/")
 def index():   
-   return render_template("index.html", title="Home")
+   return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
